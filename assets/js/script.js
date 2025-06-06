66e69c676c3e18cb8fa8f430e96ff2d785309446
kaio kampos <kaiocamposti@gmail.com>
2025-06-06 18:08:39 -0300

    Fazendo requisição, e exibindo os posts

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+async function getPosts() {
+    try {
+        let url = 'https://jsonplaceholder.typicode.com/posts';
+        const response = await fetch(url);
+        const posts = await response.json();
+
+        
+        let postsSection = document.querySelector('.posts');
+        
+        for (let post of posts) {
+            let article = document.createElement('article');
+            
+            let h2 = document.createElement('h2');
+            h2.innerHTML = post.title;
+            
+            article.appendChild(h2);
+            let p = document.createElement('p');
+            p.innerHTML = post.body;
+            
+            let button = document.createElement('button');
+            button.innerHTML = 'Like';
+            
+            let small = document.createElement('small');
+            small.innerHTML = '19 Likes';
+            
+            article.appendChild(p);
+            article.appendChild(button);
+            article.appendChild(small);
+
+            postsSection.appendChild(article);
+
+        }
+    } catch (error) {
+        // alert("Não conseguimos carregar os posts.")
+    }
+}
+
+getPosts();
